e06eab45bf7a60294a6a3aef21a9904b8cee04f1
Prakhar Gupta <prakharofficial17@gmail.com>
2025-04-26 12:12:12 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path"
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
@@ -14,6 +15,7 @@ import { app,server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = 5000 || process.env.PORT;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "3mb" }));  
 app.use(express.urlencoded({ limit: "3mb", extended: true })); 
@@ -29,6 +31,14 @@ connectDB();
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname "../frontend", "dist", "index.html"))
+    })
+}
 
 
 
